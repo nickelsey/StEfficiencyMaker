7f822f1f199d07e37a7f0ec3c53a278a83d4acbc
nickelsey <nicholas.elsey@gmail.com>
2018-08-30 15:25:31 -0400

    fixing bug in centrality definition

--- StRoot/StEfficiencyMaker/centrality_def.cc
+++ StRoot/StEfficiencyMaker/centrality_def.cc
@@ -77,7 +77,7 @@ void CentralityDef::setCentralityBounds16Bin(const std::vector<unsigned>& bounds
   
   for (unsigned i = 0; i < bounds.size(); ++i) {
     cent_bin_16_.push_back(bounds[i]);
-    if (i%2 == 0 && i < 15) {
+    if (i%2 == 0 || i == 15) {
       cent_bin_9_.push_back(bounds[i]);
     }
   }
