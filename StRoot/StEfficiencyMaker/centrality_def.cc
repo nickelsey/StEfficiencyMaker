748017303e06143401a490b4586a76d0e3e19cec
Nick Elsey <nicholas.elsey@gmail.com>
2019-07-25 20:52:30 -0400

    update cent def

--- StRoot/StEfficiencyMaker/centrality_def.cc
+++ StRoot/StEfficiencyMaker/centrality_def.cc
@@ -6,28 +6,31 @@
 
 #include "TRandom.h"
 
-CentralityDef::CentralityDef() : refmultcorr_(-1.0), centrality_16_(-1), centrality_9_(-1),
-weight_(0.0), min_vz_(-30.0), max_vz_(30.0), min_zdc_(0.0), max_zdc_(1e7), min_run_(15076101),
-max_run_(15167014), weight_bound_(400), vz_norm_(0), zdc_norm_(0) {
-  
-  zdc_par_ = std::vector<double>{181.931, -0.429191};
-  vz_par_ = std::vector<double>{542.485, 0.293356, 0.00382755, -0.000587529, -1.69114e-05, 1.0091e-06, 6.37584e-09};
-  weight_par_ = std::vector<double>{1.11444, -1.76946, 0.999062, 5.1705, -0.00127791, 416.002, 2.49573e-06};
-  std::vector<unsigned> cent_bin_16_tmp_ = std::vector<unsigned>{10, 15, 22, 31, 43, 58, 76, 97, 123, 154, 189, 230, 276, 329, 390, 459};
+CentralityDef::CentralityDef()
+    : refmultcorr_(-1.0), centrality_16_(-1), centrality_9_(-1), weight_(0.0),
+      min_vz_(-30.0), max_vz_(30.0), min_zdc_(0.0), max_zdc_(60000.0),
+      min_run_(15076101), max_run_(15167014), weight_bound_(400), vz_norm_(0),
+      zdc_norm_(30000) {
+
+  zdc_par_ = std::vector<double>{188.392, -0.32269};
+  vz_par_ =
+      std::vector<double>{529.123,      0.19706,     0.00433184,  -0.000183687,
+                          -1.29087e-05, 3.82464e-07, -1.70998e-09};
+  weight_par_ = std::vector<double>{1.34842,     -12.8629, 0.767038,   4.2547,
+                                    -0.00264771, 357.779,  5.10897e-06};
+  std::vector<unsigned> cent_bin_16_tmp_ = std::vector<unsigned>{
+      10, 15, 22, 30, 42, 56, 74, 94, 120, 149, 184, 224, 269, 321, 381, 450};
   setCentralityBounds16Bin(cent_bin_16_tmp_);
-  
 }
 
-CentralityDef::~CentralityDef() {
-  
-}
+CentralityDef::~CentralityDef() {}
 
 void CentralityDef::setEvent(int runid, double refmult, double zdc, double vz) {
   if (checkEvent(runid, refmult, zdc, vz)) {
     calculateCentrality(refmult, zdc, vz);
   }
-  // if event isn't in the run ID range, isn't in the vz range, or luminosity range,
-  // set refmultcorr = refmult
+  // if event isn't in the run ID range, isn't in the vz range, or luminosity
+  // range, set refmultcorr = refmult
   else {
     refmultcorr_ = refmult;
     centrality_9_ = -1;
@@ -39,63 +42,71 @@ void CentralityDef::setEvent(int runid, double refmult, double zdc, double vz) {
 void CentralityDef::setZDCParameters(double par0, double par1) {
   zdc_par_ = std::vector<double>{par0, par1};
 }
-void CentralityDef::setZDCParameters(const std::vector<double>& pars) {
+void CentralityDef::setZDCParameters(const std::vector<double> &pars) {
   zdc_par_.clear();
-  
+
   if (pars.size() != 2) {
     std::cerr << "zdc correction currently implemented as a linear fit "
-    << "but " << pars.size() << " parameters were passed, not 2 " << std::endl;
+              << "but " << pars.size() << " parameters were passed, not 2 "
+              << std::endl;
     return;
   }
   zdc_par_ = pars;
 }
-void CentralityDef::setVzParameters(double par0, double par1, double par2, double par3,
-                                    double par4, double par5, double par6) {
+void CentralityDef::setVzParameters(double par0, double par1, double par2,
+                                    double par3, double par4, double par5,
+                                    double par6) {
   vz_par_ = std::vector<double>{par0, par1, par2, par3, par4, par5, par6};
 }
 
-void CentralityDef::setVzParameters(const std::vector<double>& pars) {
+void CentralityDef::setVzParameters(const std::vector<double> &pars) {
   vz_par_.clear();
-  
+
   if (pars.size() != 7) {
-    std::cerr << "vz correction currently implemented as a 6th order polynomial "
-    << "but " << pars.size() << " parameters were passed, not 7 " << std::endl;;
+    std::cerr
+        << "vz correction currently implemented as a 6th order polynomial "
+        << "but " << pars.size() << " parameters were passed, not 7 "
+        << std::endl;
+    ;
     return;
   }
   vz_par_ = pars;
 }
 
-void CentralityDef::setCentralityBounds16Bin(const std::vector<unsigned>& bounds) {
+void CentralityDef::setCentralityBounds16Bin(
+    const std::vector<unsigned> &bounds) {
   cent_bin_16_.clear();
   cent_bin_9_.clear();
-  
+
   if (bounds.size() != 16) {
     std::cerr << "centrality bounds require 16 parameters, but only "
-    << bounds.size() << " parameters were passed" << std::endl;
+              << bounds.size() << " parameters were passed" << std::endl;
     return;
   }
-  
+
   for (unsigned i = 0; i < bounds.size(); ++i) {
     cent_bin_16_.push_back(bounds[i]);
-    if (i%2 == 0 || i == 15) {
+    if (i % 2 == 0 || i == 15) {
       cent_bin_9_.push_back(bounds[i]);
     }
   }
 }
 
-void CentralityDef::setWeightParameters(const std::vector<double>& pars, double bound) {
+void CentralityDef::setWeightParameters(const std::vector<double> &pars,
+                                        double bound) {
   weight_par_.clear();
   weight_bound_ = 0;
   if (pars.size() != 7) {
-    std::cerr << "reweighting require 7 parameters, but only "
-    << pars.size() << " parameters were passed" << std::endl;
+    std::cerr << "reweighting require 7 parameters, but only " << pars.size()
+              << " parameters were passed" << std::endl;
     return;
   }
   weight_par_ = pars;
   weight_bound_ = bound;
 }
 
-bool CentralityDef::checkEvent(int runid, double refmult, double zdc, double vz) {
+bool CentralityDef::checkEvent(int runid, double refmult, double zdc,
+                               double vz) {
   if (refmult < 0)
     return false;
   if (max_run_ > 0 && (runid < min_run_ || runid > max_run_))
@@ -108,37 +119,40 @@ bool CentralityDef::checkEvent(int runid, double refmult, double zdc, double vz)
 }
 
 void CentralityDef::calculateCentrality(double refmult, double zdc, double vz) {
-  
-  // we randomize raw refmult within 1 bin to avoid the peaky structures at low refmult
+
+  // we randomize raw refmult within 1 bin to avoid the peaky structures at low
+  // refmult
   double raw_ref = refmult + gRandom->Rndm();
-  
+
   if (zdc_par_.empty() || vz_par_.empty()) {
-    std::cerr << "zdc and vz correction parameters must be set before refmultcorr can be calculated" << std::endl;
+    std::cerr << "zdc and vz correction parameters must be set before "
+                 "refmultcorr can be calculated"
+              << std::endl;
     refmultcorr_ = 0.0;
     centrality_9_ = -1;
     centrality_16_ = -1;
     weight_ = 0.0;
   }
-  
+
   double zdc_scaling = zdc_par_[0] + zdc_par_[1] * zdc / 1000.0;
   double zdc_norm = zdc_par_[0] + zdc_par_[1] * zdc_norm_ / 1000.0;
-  
+
   double zdc_correction = zdc_norm / zdc_scaling;
-  
+
   double vz_scaling = 0.0;
   double vz_norm = 0.0;
   for (int i = 0; i < vz_par_.size(); ++i) {
     vz_scaling += vz_par_[i] * pow(vz, i);
     vz_norm += vz_par_[i] * pow(vz_norm_, i);
   }
-  
+
   double vz_correction = 1.0;
   if (vz_scaling > 0.0) {
     vz_correction = vz_norm / vz_scaling;
   }
-  
+
   refmultcorr_ = raw_ref * vz_correction * zdc_correction;
-  
+
   // now calculate the centrality bins, both 16 & 9
   centrality_9_ = -1;
   for (int i = 0; i < cent_bin_9_.size(); ++i) {
@@ -147,7 +161,7 @@ void CentralityDef::calculateCentrality(double refmult, double zdc, double vz) {
       break;
     }
   }
-  
+
   centrality_16_ = -1;
   for (int i = 0; i < cent_bin_16_.size(); ++i) {
     if (refmultcorr_ >= cent_bin_16_[cent_bin_16_.size() - i - 1]) {
@@ -155,9 +169,10 @@ void CentralityDef::calculateCentrality(double refmult, double zdc, double vz) {
       break;
     }
   }
-  
+
   // now get the weight
-  if (weight_par_.size() && centrality_9_ >= 0 && centrality_16_ >= 0 && refmultcorr_ < weight_bound_) {
+  if (weight_par_.size() && centrality_9_ >= 0 && centrality_16_ >= 0 &&
+      refmultcorr_ < weight_bound_) {
     double par0 = weight_par_[0];
     double par1 = weight_par_[1];
     double par2 = weight_par_[2];
@@ -166,10 +181,9 @@ void CentralityDef::calculateCentrality(double refmult, double zdc, double vz) {
     double par5 = weight_par_[5];
     double par6 = weight_par_[6];
     double ref_const = refmultcorr_ * par2 + par3;
-    weight_ = par0 + par1 / ref_const + par4 * ref_const + par5 / pow(ref_const, 2.0) + par6 * pow(ref_const, 2.0);
-  }
-  else {
+    weight_ = par0 + par1 / ref_const + par4 * ref_const +
+              par5 / pow(ref_const, 2.0) + par6 * pow(ref_const, 2.0);
+  } else {
     weight_ = 1.0;
   }
 }
-
